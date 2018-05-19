f048c9065928a93a40efd884257fb770d7e43a84
Maxson Sayner <max.sayner@gmail.com>
2018-05-19 12:11:49 -0600

    Done

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './components/Image';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <Image myImage={"http://www.fz09.org/forum/attachments/122001d1508166584-any-news-2018-yamaha-850-mt-09-street-rally-2016-700px.jpg"}/>
       </div>
     );
   }
